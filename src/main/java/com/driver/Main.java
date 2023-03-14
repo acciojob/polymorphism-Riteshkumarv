ef7b4fcb84bd0f86f2c86e590673685776d6e416
ritesh kumar <Ritesh74461@gmail.com>
2023-03-14 09:37:25 +0530

    new code2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,16 +9,15 @@ public class Main {
         double ans3 = p.product(1.2,2.3);
         System.out.print(ans1+" "+ans2+" "+ans3);
     }
-
-}
-    class Product {
-    public int product(int x , int y){
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-        return x*y*z;
-    }
-    public double product(double x,double y) {
-        return x * y;
+    public static class Product {
+        public int product(int x , int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y) {
+            return x * y;
+        }
     }
 }
