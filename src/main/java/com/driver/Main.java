2ce4077ca5ee3c45c407c30fa433592a741fc41f
ritesh kumar <Ritesh74461@gmail.com>
2023-03-14 09:14:42 +0530

    Assinment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    static class Product {
+        public int product(int x , int y){
+            return x+y;
+        }
+        public int product(int x,int y,int z){
+            return x+y+z;
+        }
+        public double product(double x,double y) {
+            return x + y;
+        }
+    }
+    public static void main(String []args){
+        Product p = new Product();
+        int ans1 = p.product(1,2);
+        int ans2 = p.product(1,2,3);
+        double ans3 = p.product(1.2,2.3);
+        System.out.print(ans1+" "+ans2+" "+ans3);
+    }
 }
